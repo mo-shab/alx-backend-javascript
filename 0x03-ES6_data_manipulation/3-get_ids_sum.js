712e9a1030303f903a819655d5ea35b313e88a3a
Mohammed <mrcloud.med@gmail.com>
2024-07-29 23:47:52 +0000

    Task from 2 to 4

---
+++ 0x03-ES6_data_manipulation/3-get_ids_sum.js
@@ -0,0 +1,8 @@
+const getStudentIdsSum = (students) => {
+    if (!Array.isArray(students)) {
+        return 0;
+    }
+    return students.map((student) => student.id).reduce((prev, next) => prev + next);
+}
+
+export default getStudentIdsSum;
