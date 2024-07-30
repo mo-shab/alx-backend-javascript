ae8f9619b7670c1a4f7986e1170bf1dcbef7e270
Mohammed <mrcloud.med@gmail.com>
2024-07-30 12:51:05 +0000

    task0

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,21 +1,7 @@
 export default function getListStudents() {
-  const student_1 = {
-    id: 1,
-    firstName: 'Guillaume',
-    location: 'San Francisco',
-  };
-
-  const student_2 = {
-    id: 2,
-    firstName: 'James',
-    location: 'Columbia',
-  };
-
-  const student_3 = {
-    id: 5,
-    firstName: 'Serena',
-    location: 'San Francisco',
-  };
-
-  return [student_1, student_2, student_3];
+return [
+  { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
+  { id: 2, firstName: 'James', location: 'Columbia' },
+  { id: 5, firstName: 'Serena', location: 'San Francisco' }
+];
 }
