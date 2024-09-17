c2fdceedcbd040f1f51386523b49bfdc998880be
Mohammed <mrcloud.med@gmail.com>
2024-09-17 21:08:52 +0000

    Task 1

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,14 +1,9 @@
-#!/usr/bin/env node
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
-console.log('Welcome to Holberton School, what is your name?');
-
-process.stdin.on('data', (input) => {
-    const name = input.toString().trim();
-    console.log(`Your name is: ${name}`);
-
-    process.stdin.end();
+process.stdin.on('data', (data) => {
+  process.stdout.write(`Your name is: ${data}`);
 });
 
 process.stdin.on('end', () => {
-    console.log('This important software is now closing');
+  process.stdout.write('This important software is now closing\n');
 });
